ce05b3ce45a2af6ec97dc85f6d1d069107f4b42e
Carlos Soto <carlosdedios0621@gmail.com>
2022-02-27 21:25:23 -0400

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,42 @@
+//Servidor de sockets
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
+const Sockets = require("./sockets");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    //Http server
+    this.server = http.createServer(this.app);
+
+    //Configurando Sockets
+    this.io = socketio(this.server);
+  }
+
+  middlewares() {
+    //Desplegar el directorio publico
+    this.app.use(express.static(path.resolve(__dirname, "../public")));
+  }
+
+  configurarSockets() {
+    new Sockets(this.io);
+  }
+
+  execute() {
+    //Inicializar Middlewares
+    this.middlewares();
+    //Inicializar Sockets
+    this.configurarSockets();
+
+    //Iniciarlizar Server
+    this.server.listen(this.port, () => {
+      console.log("Server corriendo en puerto: ", this.port);
+    });
+  }
+}
+
+module.exports = Server;
